0d2c357de8fdbd77d1c3da203b0d0d143ea9d769
sentobc13 <sentoberlanga@gmail.com>
2024-05-21 15:08:22 +0200

    comonents artists creado

---
+++ ejercicio-react-3-repaso/src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+
+function App() {
+  const songs = [
+    {
+      id: 1,
+      title: 'Song 1',
+      artist: 'Artist 1',
+      album: 'Album 1',
+      duration: '3:45'
+    },
+    {
+      id: 2,
+      title: 'Song 2',
+      artist: 'Artist 2',
+      album: 'Album 2',
+      duration: '4:20'
+    },
+    {
+      id: 3,
+      title: 'Song 3',
+      artist: 'Artist 3',
+      album: 'Album 3',
+      duration: '5:10'
+    }
+  ]
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
